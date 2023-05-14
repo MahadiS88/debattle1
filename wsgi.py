c5017e2d10a8e53ded759fd1c1c4232f36531f2b
MahadiS88 <133576802+MahadiS88@users.noreply.github.com>
2023-05-14 19:37:43 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debattle.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'debattle1.settings')
 
 application = get_wsgi_application()
